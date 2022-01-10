9c6718c90a8d73f0ced15d4ad1e139c8b199179f
ilyDark <65448134+dacoev@users.noreply.github.com>
2022-01-10 12:06:07 +0900

    types

---
+++ app.js
@@ -0,0 +1,11 @@
+function add(n1, n2, showResult, phrase) {
+    if (typeof n1 !== "number" || typeof n2 !== "number") {
+        throw new Error("Incorrect input!");
+    }
+}
+var number1;
+number1 = 5;
+var number2 = true;
+var printResult = true;
+var resultPhrase = "Result is: ";
+add(number1, number2, printResult, resultPhrase);
