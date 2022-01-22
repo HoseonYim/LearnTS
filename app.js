154fe3e8af75c7982831f215cd05bf8a2997e6e6
ilyDark <65448134+dacoev@users.noreply.github.com>
2022-01-23 00:30:53 +0900

    Enum & Any

--- app.js
+++ app.js
@@ -2,18 +2,38 @@
     name: string;
     age: number;
 } = {*/
+/*const person: {
+    name:string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {*/
+//const ADMIN = 0;
+//const READ_ONLY = 1;
+//const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 10] = "ADMIN";
+    Role[Role["READ_ONLY"] = 11] = "READ_ONLY";
+    Role["AUTHOR"] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var person = {
     name: "Fuck",
     age: 100,
     hobbies: ["Sports", "Cooking"],
-    role: [2, "author"]
+    role: Role.AUTHOR
 };
-console.log(person.role.push("admin"));
-person.role[1] = 10;
+//person.role.push("admin"); //tuple cant catch this error
+//person.role[1] = 10;
+//person.role = [0, 'admin', 'user'];
 var favoriteActivities = ["Cooking"];
 favoriteActivities = ["Sports", 1];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby);
+}
+if (person.role === Role.AUTHOR) {
+    console.log("is author");
 }
--- app.ts
+++ app.ts
@@ -2,15 +2,34 @@
     name: string;
     age: number;
 } = {*/
+
+/*const person: {
+    name:string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {*/
+
+//const ADMIN = 0;
+//const READ_ONLY = 1;
+//const AUTHOR = 2;
+
+enum Role {
+  ADMIN = 10,
+  READ_ONLY,
+  AUTHOR = "AUTHOR"
+};
+
 const person = {
   name: "Fuck",
   age: 100,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: Role.AUTHOR,
 };
 
-person.role.push("admin");
-person.role[1] = 10;
+//person.role.push("admin"); //tuple cant catch this error
+//person.role[1] = 10;
+//person.role = [0, 'admin', 'user'];
 
 let favoriteActivities: any[] = ["Cooking"];
 favoriteActivities = ["Sports", 1];
@@ -19,4 +38,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby);
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log("is author");
 }
