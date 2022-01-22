4e84ce5595b4eb6bb2db947b0c686f2ee58380e5
ilyDark <65448134+dacoev@users.noreply.github.com>
2022-01-22 19:03:00 +0900

    !

--- app.js
+++ app.js
@@ -3,12 +3,15 @@
     age: number;
 } = {*/
 var person = {
-    name: 'Fuck',
+    name: "Fuck",
     age: 100,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ["Sports", "Cooking"],
+    role: [2, "author"]
 };
-var favoriteActivities = ['Cooking'];
-favoriteActivities = ['Sports', 1];
+console.log(person.role.push("admin"));
+person.role[1] = 10;
+var favoriteActivities = ["Cooking"];
+favoriteActivities = ["Sports", 1];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
--- app.ts
+++ app.ts
@@ -3,16 +3,20 @@
     age: number;
 } = {*/
 const person = {
-    name: 'Fuck',
-    age: 100,
-    hobbies: ['Sports', 'Cooking']
+  name: "Fuck",
+  age: 100,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
 
-let favoriteActivities: any[] = ['Cooking'];
-favoriteActivities = ['Sports', 1];
+person.role.push("admin");
+person.role[1] = 10;
+
+let favoriteActivities: any[] = ["Cooking"];
+favoriteActivities = ["Sports", 1];
 
 console.log(person.name);
 
-for(const hobby of person.hobbies){
-    console.log(hobby);
+for (const hobby of person.hobbies) {
+  console.log(hobby);
 }
