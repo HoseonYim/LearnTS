4a5b366dd2533e75a25f4c55eb22af2860659b13
ilyDark <65448134+dacoev@users.noreply.github.com>
2022-01-30 19:57:12 +0900

    Literal Types

--- app.js
+++ app.js
@@ -1,42 +1,23 @@
-/*const person: {
-    name: string;
-    age: number;
-} = {*/
-/*const person: {
-    name:string;
-    age: number;
-    hobbies: string[];
-    role: [number, string];
-} = {*/
-//const ADMIN = 0;
-//const READ_ONLY = 1;
-//const AUTHOR = 2;
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 10] = "ADMIN";
-    Role[Role["READ_ONLY"] = 11] = "READ_ONLY";
-    Role["AUTHOR"] = "AUTHOR";
-})(Role || (Role = {}));
-;
-var person = {
-    name: "Fuck",
-    age: 100,
-    hobbies: ["Sports", "Cooking"],
-    role: Role.AUTHOR
-};
-//person.role.push("admin"); //tuple cant catch this error
-//person.role[1] = 10;
-//person.role = [0, 'admin', 'user'];
-var favoriteActivities = ["Cooking"];
-favoriteActivities = ["Sports", 1];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby);
+function combine(input1, input2, resultConversion) {
+    var result;
+    //result: number | string = input1 + input2
+    if ((typeof input1 === 'number' && typeof input2 === 'number') ||
+        resultConversion === 'as-number') {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    //if (resultConversion === 'as-number') {
+    //	return parseFloat(result);
+    //} else {
+    //	return result.toString();
+    //}
+    return result;
 }
-if (person.role === Role.AUTHOR) {
-    console.log("is author");
-}
-var dontUseThisPlease = 5;
-dontUseThisPlease = 'plzz';
-//let betterThanAny: any[] = 5;//error beacuse it isnt array
+var combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
+var combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+var combinedNames = combine('Max', 'Anna', 'as-text');
+console.log(combinedNames);
