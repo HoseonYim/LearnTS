6bebd157a15d8efe67caed607eba7f1b61707581
ilyDark <65448134+dacoev@users.noreply.github.com>
2022-01-30 18:16:52 +0900

    i meant this

--- app.ts
+++ app.ts
@@ -1,6 +1,11 @@
 function combine(input1: number | string, input2: number | string) {
 	let result;
 	//result: number | string = input1 + input2
+	if (typeof input1 === 'number' && typeof input2 === 'number') {
+		result = input1 + input2;
+	} else {
+		result = input1.toString() + input2.toString();
+	}
 	return result;
 }
 
