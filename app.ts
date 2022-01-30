4a4a8edf85e1ad0875d3ce07a737a9ad6048bb1f
ilyDark <65448134+dacoev@users.noreply.github.com>
2022-01-30 20:48:31 +0900

    Function Types and Callbacks

--- app.ts
+++ app.ts
@@ -2,8 +2,29 @@ function add(n1: number, n2: number): number {
 	return n1 + n2;
 }
 
-function printResult(num: number): void {//undefined need return;
+function printResult(num: number): void {
+	//undefined need return;
 	console.log('Result: ' + num);
 }
 
-console.log(printResult(add(5, 12)));
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+	const result = n1 + n2;
+	cb(result);
+}
+
+printResult(add(5, 12));
+
+let combineValues: (a: number, b: number) => number;
+//the variable's name doesn't matter
+//there is also function type
+
+combineValues = add;
+//combineValues = printResult;
+//combineValues = 5;
+
+console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+	console.log(result);
+	//return result;
+});
